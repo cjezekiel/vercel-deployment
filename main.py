4b38de68be49c6850be71563496c2b920b1c8a4a
cjezekiel <55980422+cjezekiel@users.noreply.github.com>
2026-01-03 09:17:05 +0100

    Vercel fastapi deployment
    Just good ol' Vercel test deployment

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+db = [
+    { "name": "Noma", "age": 23 },
+    { "name": "Alvon", "age": 16 },
+    { "name": "Pinto", "age": 32 },
+    { "name": "Dona", "age": 38 },
+    { "name": "Cairo", "age": 17 },
+]
+
+
+@app.get("/")
+async def home_root():
+    return db
+
+@app.get("/deploy")
+async def home_root():
+    return {"message": "Render Deploy"}
